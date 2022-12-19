a48d781f43406765b8db25a3ae3de8c721aa63d0
Aqibaman <aqibaman0173@gmail.com>
2022-12-19 17:30:00 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# DevLearn
+
+Devlearn project Link: [DevLearn](https://devlearn-afc3d.web.app).
+
+## Devlearn Scripts
+
+DevLearn is a company which will help students to build better tech company.
+
+### `Here we use Tailwind CSS framework`
+
+### `Here we use firebase for deploy`
+
+### `Vercel is used for server side deploy`
+
+### `We use lottie-animations`
+
+### `React router dom for routing`
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const coursesDetails = require('./data/coursesDetails.json');
+
+app.get('/', (req, res) => {
+    res.send('DevLearn API Running');
+});
+
+app.get('/category', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === "01") {
+        console.log(id)
+        res.send(coursesDetails)
+    }
+    else {
+        const category_course = coursesDetails.filter(c => c.category_id === id);
+        res.send(category_course);
+    }
+
+})
+
+
+
+app.get('/courses', (req, res) => {
+    res.send(coursesDetails);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === "01") {
+        console.log(id)
+        res.send(coursesDetails)
+    }
+    else {
+        const selectedCourses = coursesDetails.find(c => c.id === id);
+        res.send(selectedCourses);
+    }
+});
+
+app.listen(port, () => {
+    console.log('DevLearn Api Running', port);
+})
